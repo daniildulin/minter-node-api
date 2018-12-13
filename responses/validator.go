e326dc45d84f571d8036df97de638e2fdffe9696
Daniil Dulin <daniildulin@gmail.com>
2018-12-13 12:15:10 +0300

    update ValidatorsResponse

--- responses/validator.go
+++ responses/validator.go
@@ -2,11 +2,11 @@ package responses
 
 type ValidatorsResponse struct {
 	Response
-	Signed []Validator `json:"result"`
+	Validators []Validator `json:"result"`
 }
 
 type Validator struct {
 	PubKey      string  `json:"pubkey"`
-	Signed      bool    `json:"signed"`
+	Signed      *bool   `json:"signed"`
 	VotingPower *string `json:"voting_power"`
 }
