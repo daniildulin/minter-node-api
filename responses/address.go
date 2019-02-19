337e3ecc1387431c4044e13ac97ea54dff65141e
Daniil Dulin <daniildulin@gmail.com>
2019-02-19 16:32:18 +0300

    add balance request by a few addresses

--- responses/address.go
+++ responses/address.go
@@ -2,8 +2,5 @@ package responses
 
 type AddressResponse struct {
 	Response
-	Result struct {
-		TransactionCount string            `json:"transaction_count"`
-		Balance          map[string]string `json:"balance"`
-	} `json:"result"`
+	Result Balance `json:"result"`
 }
