d640737148456fda73c8a6ed8e4d364cb140faa4
Daniil Dulin <daniildulin@gmail.com>
2019-03-22 16:29:28 +0300

    fix candidates with stakes

--- responses/candidate.go
+++ responses/candidate.go
@@ -18,13 +18,14 @@ type CandidateResponse struct {
 type BlockCandidatesResponse struct {
 	Response
 	Result []struct {
-		RewardAddress  string `json:"reward_address"`
-		OwnerAddress   string `json:"owner_address"`
-		TotalStake     string `json:"total_stake"`
-		PubKey         string `json:"pubkey"`
-		Commission     string `json:"commission"`
-		CreatedAtBlock string `json:"created_at_block"`
-		Status         uint8  `json:"status"`
+		RewardAddress  string  `json:"reward_address"`
+		OwnerAddress   string  `json:"owner_address"`
+		TotalStake     string  `json:"total_stake"`
+		PubKey         string  `json:"pubkey"`
+		Commission     string  `json:"commission"`
+		CreatedAtBlock string  `json:"created_at_block"`
+		Status         uint8   `json:"status"`
+		Stakes         []Stake `json:"stakes"`
 	} `json:"result"`
 }
 
